05c68c488f2ed202dc17fdb4f6cf22e3616a3362
Oleksii <51969410+djighoul29@users.noreply.github.com>
2021-05-23 17:50:13 +0300

    Edited cactus spawner
    New spawn rate every frame

--- Assets/Scripts/CactusSpawner.cs
+++ Assets/Scripts/CactusSpawner.cs
@@ -7,16 +7,22 @@ public class CactusSpawner : MonoBehaviour
 	[SerializeField]
 	Transform spawnPoint;
 	[SerializeField]
-	float spawnRate = 2f;
-	private float nextSpawn;
-	void Update()
+	private float spawnRateMin = 0.75f;
+	[SerializeField]
+	private float spawnRateMax = 1.1f;
+	private float spawnRate = 1f;
+	private float nextSpawn = 1f;
+
+    void Update()
 	{
+		spawnRate = Random.Range(spawnRateMin, spawnRateMax);
 		if (Time.time > nextSpawn)
 			SpawnObstacle();
 	}
 	void SpawnObstacle()
 	{
 		nextSpawn = Time.time + spawnRate;
+		Debug.Log(nextSpawn);
 		int randomObstacle = Random.Range(0, obstacles.Length);
 		Instantiate(obstacles[randomObstacle], spawnPoint.position, Quaternion.identity);
 	}
